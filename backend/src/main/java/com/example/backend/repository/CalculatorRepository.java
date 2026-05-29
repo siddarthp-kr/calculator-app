c37a9fb9cb989d88a78e5ad81ee881cf0dc9f3a9
Jeev Sampath <jeev.sampath@kroger.com>
2026-05-29 16:28:40 -0400

    Deleting unused code

--- backend/src/main/java/com/example/backend/repository/CalculatorRepository.java
+++ backend/src/main/java/com/example/backend/repository/CalculatorRepository.java
@@ -1,10 +1,7 @@
 package com.example.backend.repository;
 
 import com.example.backend.model.Calculation;
-import java.util.List;
 
 public interface CalculatorRepository {
-    int save(Calculation calculation);
-    List<Calculation> historyPagination(int page, int size);
-    
+    int save(Calculation calculation);    
 }
--- backend/src/main/java/com/example/backend/repository/CalculatorRepositoryImpl.java
+++ backend/src/main/java/com/example/backend/repository/CalculatorRepositoryImpl.java
@@ -6,7 +6,6 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import java.util.Objects;
-import java.util.List;
 
 @Repository
 public class CalculatorRepositoryImpl implements CalculatorRepository {
@@ -16,8 +15,7 @@ public class CalculatorRepositoryImpl implements CalculatorRepository {
     private static final String RESULT = "RESULT";
     //Insert new row for calculations table containing values
     private static final String SQL_INSERT_CALCULATION = "INSERT INTO calculation (num1, num2, operation, result) " + "VALUES (:NUM1, :NUM2, :OPERATION, :RESULT)";
-    private static final String SQL_SELECT_PAGINATION = "SELECT * FROM calculation ORDER BY id LIMIT :SIZE OFFSET :OFFSET";
-
+    
     private final JdbcTemplate jdbcTemplate;
 
     public CalculatorRepositoryImpl(JdbcTemplate jdbcTemplate) {
@@ -33,29 +31,6 @@ public class CalculatorRepositoryImpl implements CalculatorRepository {
             .addValue(RESULT, calculation.getResult());
         
         return new NamedParameterJdbcTemplate(Objects.requireNonNull(jdbcTemplate.getDataSource())).update(SQL_INSERT_CALCULATION, parameterSource);
-    }
-
-    @Override
-    public List<Calculation> historyPagination(int page, int size) {
-        int offset = page * size;
-
-        MapSqlParameterSource parameterSource = new MapSqlParameterSource()
-            .addValue("SIZE", size)
-            .addValue("OFFSET", offset);
-
-        return new NamedParameterJdbcTemplate(Objects.requireNonNull(jdbcTemplate.getDataSource())).query(SQL_SELECT_PAGINATION, parameterSource, (result, rowNum) -> {
-            Calculation c  = new Calculation();
-
-            c.setId(result.getInt("id"));
-            c.setNum1(result.getDouble("num1"));
-            c.setNum2(result.getDouble("num2"));
-            c.setOperation(result.getString("operation"));
-            c.setResult(result.getDouble("result"));
-
-            return c;
-
-        });
-
     }
 
 }
