992be0d2fa3ecbbeb40226ad16dbe6738b78128c
Jeev Sampath <jeev.sampath@kroger.com>
2026-05-29 15:25:04 -0400

    Sorted response list in reverse chronological order

--- backend/src/main/java/com/example/backend/model/CalculationHistoryResponse.java
+++ backend/src/main/java/com/example/backend/model/CalculationHistoryResponse.java
@@ -2,6 +2,7 @@ package com.example.backend.model;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import java.util.Comparator;
 
 @Getter
 @Setter
@@ -10,5 +11,20 @@ public class CalculationHistoryResponse {
 
     public CalculationHistoryResponse(List<Calculation> calculations){
         this.calculations = calculations;
+        //this sorts the list so that the History page lists calculations in reverse chronological order
+        this.calculations.sort(new CalculationComparator());
+    }
+
+    public class CalculationComparator implements Comparator<Calculation> {
+        @Override
+        public int compare(Calculation a, Calculation b){
+            int result = 0;
+            if(a.getId() > b.getId()){
+                result = -1;
+            } else if(b.getId() > a.getId()) {
+                result = 1;
+            }
+            return result;
+        }
     }
 }
